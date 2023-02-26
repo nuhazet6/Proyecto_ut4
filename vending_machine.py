892dbca8453c4c560abf34d9848ee47fa56f5774
javigh1903 <javigh1903@movistar.es>
2023-02-26 15:15:56 +0000

    sesión conjunta nº1

--- vending_machine.py
+++ vending_machine.py
@@ -1,7 +1,7 @@
 with open("vending.dat", "r") as f:
     TYPE_COIN = [2, 1, 0.5]
     coins = {
-        type: float(coin) for type, coin in zip(TYPE_COIN, f.readline().strip().split())
+        coin_type: float(coin) for coin_type, coin in zip(TYPE_COIN, f.readline().strip().split())
     }
     products = {}
     for line in f:
@@ -13,31 +13,63 @@ with open("vending.dat", "r") as f:
 
 with open("operations.dat", "r") as f:
     orders = []
-    codes = set()
     for line in f:
         info_envio = line.strip().split()
-        order_type = info_envio[0]
-        code = info_envio[1]
-        amount = int(info_envio[2])
-        money = [int(i) for i in info_envio[3:]]
-        orders.append((order_type, code, amount, money))
-print(products, coins)
+        orders.append(info_envio)
 
-
-def price(code):
-    info = products.get(code, {"ERROR": -1})
-    price = list(info.items()[0])
+def get_price(code):
+    info = products.get(code, {})
+    price = info.get('price', -1)#ERROR: -1
     return price
 
-
-def stock(code):
-    info = products.get(code, {None: None, "ERROR": -2})
-    stock = list(info.items())[1]
+def get_stock(code):
+    info = products.get(code, {})
+    stock = info.get('stock', -2)#ERROR: -2
     return stock
 
+def get_coin(coin_type):
+    return  coins.get(coin_type, -3)
 
-print(stock("D31"))
+def change_coins(change):
+    change_amnts = {}
+    for coin_type in TYPE_COIN:
+        change_amnts[coin_type] = change // coin_type 
+        if change_amnts[coin_type] > coins[coin_type]:
+            change_amnts[coin_type] = coins[coin_type]#logica de devolución?
+        change -= change_amnts[coin_type]
+    return change_amnts
+    
+def do_order(operation_data):
+    code, quant, *amnt_coins = operation_data
+    price = get_price(code)
+    stock = get_stock(code)
+    payed = sum([value*int(amnt) for value,amnt in zip(TYPE_COIN,amnt_coins)])
+    for coin_type,amnt in zip(TYPE_COIN, amnt_coins):
+        coins[coin_type] += amnt   
+    change = payed - price * quant#antes o después de introducir sus monedas en el balance?
+    change_amnts = change_coins(change)
+    change = (change == sum([k*v for k,v in change_amnts.items()]))-1#hacerselo mirar(dejarla por ahí en cualquier caso)
+    stock_movement = stock - quant
+    error_catched = '-' in (str(price) + str(stock) + str(change) + str(stock_movement))#función? n argumentos iterados y sumados como str
+    if error_catched:
+        for coin_type,amnt in zip(TYPE_COIN, amnt_coins):
+            coins[coin_type] -= amnt 
+    else:
+        change_amnts = change_coins(change)
+        for coin_type in (TYPE_COIN):
+            coins[coin_type] -= change_amnts[coin_type]
+    return 
 
+for order in orders:
+    operation_type, operation_data = order[0], order[1:]
+    match operation_type:
+        case 'O':
+        case 'R':
+        case 'P':
+        case 'C':
+        case _: #capturar error de operación no soportada
 
-def buy(*, code, amount, money):
-    return 0
+#recarga de producto  N
+#cambio precio  J
+#recarga monedas  J
+#salida N
